f9ab5b7a7dfef1acf8f1d97ea0200f931d1e3710
李志刚 <zhigang.li@oriente.com>
2020-04-07 18:26:16 +0800

    simple javapoet is used in conjunction with auto-service

--- app/src/main/java/com/oriente/aptdemo/MainActivity.java
+++ app/src/main/java/com/oriente/aptdemo/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.oriente.annotation.Flag;
+
+@Flag
 public class MainActivity extends AppCompatActivity {
 
     @Override
---
+++ compiler/src/main/java/com/oriente/compiler/FlagAnnotationProcessor.java
@@ -0,0 +1,59 @@
+package com.oriente.compiler;
+
+import com.google.auto.service.AutoService;
+import com.oriente.annotation.Flag;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
+
+import java.io.IOException;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Processor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.TypeElement;
+
+@AutoService(Processor.class)
+public class FlagAnnotationProcessor extends AbstractProcessor {
+
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return SourceVersion.latestSupported();
+    }
+
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        LinkedHashSet<String> types = new LinkedHashSet<>();
+        types.add((Flag.class.getCanonicalName()));
+        return types;
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
+        MethodSpec main = MethodSpec.methodBuilder("main")
+                .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+                .returns(void.class)
+                .addParameter(String[].class, "args")
+                .addStatement("$T.out.println($S)", System.class, "Hello,JavaPoet!")
+                .build();
+
+        TypeSpec helloWorld = TypeSpec.classBuilder("HelloWorld")
+                .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+                .addMethod(main)
+                .build();
+
+        JavaFile javaFile = JavaFile.builder("com.oriente.aptdemo", helloWorld)
+                .build();
+        try {
+            javaFile.writeTo(processingEnv.getFiler());
+        } catch (IOException e) {
+            e.printStackTrace();
+        };
+
+        return true;
+    }
+}
